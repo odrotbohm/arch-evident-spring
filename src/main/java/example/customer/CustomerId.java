632b8f43447a6a9ba89624b5ca7f505b47380d54
Oliver Drotbohm <odrotbohm@vmware.com>
2022-06-01 17:01:01 +0200

    Initial commit.

---
+++ src/main/java/example/customer/CustomerId.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2022 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package example.customer;
+
+import jakarta.persistence.Embeddable;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+@Embeddable
+@EqualsAndHashCode
+@RequiredArgsConstructor(staticName = "of")
+@NoArgsConstructor(force = true)
+public class CustomerId implements Serializable {
+
+	private static final long serialVersionUID = 1009997590119941755L;
+	private final UUID id;
+}
---
+++ src/test/java/example/order/OrderIntegrationTests.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2022 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package example.order;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import example.customer.CustomerId;
+import example.inventory.Inventory;
+import lombok.RequiredArgsConstructor;
+
+import java.util.UUID;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.modulith.test.ApplicationModuleTest;
+
+/**
+ * @author Oliver Drotbohm
+ */
+@ApplicationModuleTest
+@RequiredArgsConstructor
+class OrderIntegrationTests {
+
+	private final OrderManagement orders;
+	private final OrderRepository repository;
+
+	@MockBean Inventory inventory;
+
+	@Test
+	void bootstrapsOrderModule() {
+
+	}
+
+	@Test
+	void persistsAndLoadsOrder() {
+
+		var reference = repository.save(new Order(CustomerId.of(UUID.randomUUID())));
+		var result = repository.findById(reference.getId());
+
+		// Equal but not the same
+		assertThat(result).hasValue(reference);
+		assertThat(result).hasValueSatisfying(it -> assertThat(it).isNotSameAs(reference));
+	}
+
+	@Test
+	void orderCompletionTriggersInventoryUpdate() {
+
+		var order = new Order(CustomerId.of(UUID.randomUUID()));
+
+		orders.complete(order);
+
+		verify(inventory).updateStock();
+	}
+}
