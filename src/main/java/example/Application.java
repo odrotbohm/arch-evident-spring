4895655aaa5b3765c611458e293170d3a4aed57e
Oliver Drotbohm <odrotbohm@vmware.com>
2022-07-06 22:01:33 +0200

    Break module cycle using events.

--- src/main/java/example/Application.java
+++ src/main/java/example/Application.java
@@ -3,12 +3,14 @@ package example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 /**
  * Spring for the architecturally curious developer
  *
  * @author Oliver Drotbohm (@odrotbohm)
  */
+@EnableAsync
 @SpringBootApplication
 @ConfigurationPropertiesScan
 public class Application {
